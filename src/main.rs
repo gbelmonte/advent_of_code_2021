cd238e2d8a78b54a9931b9d3b0eacdbcf80a0230
Greg <greg.j.belmonte@gmail.com>
2021-12-04 10:26:10 -0800

    day 4 solution

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, borrow::BorrowMut, collections::HashSet};
 
 fn main() {
     day_1_part_1_solution();
@@ -7,6 +7,8 @@ fn main() {
     day_2_part_2_solution();
     day_3_part_1_solution();
     day_3_part_2_solution();
+    day_4_part_1_solution();
+    day_4_part_2_solution();
 }
 
 fn day_1_part_1_solution() {
@@ -153,4 +155,184 @@ fn day_3_part_2_solution() {
     let oxygen_generator_rating = u32::from_str_radix(&oxygen_generator.iter().collect::<String>(), 2).unwrap();
     let co2_scrubber_rating = u32::from_str_radix(&co2_scrubber.iter().collect::<String>(), 2).unwrap();
     println!("life support rating {}", oxygen_generator_rating * co2_scrubber_rating);
+}
+
+fn day_4_part_1_solution() {
+    let mut input: Vec<String> = fs::read_to_string("input/day_4/part1.txt")
+        .expect("Something went wrong reading the file")
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(String::from)
+        .collect();
+
+    let called_numbers: Vec<i32> = input.remove(0)
+        .split(",")
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+
+    let mut boards: Vec<Vec<Vec<i32>>> = input.chunks(5)
+        .map(|board| {
+            board.iter()
+                .map(|row| {
+                    row.split_whitespace()
+                        .map(|val| val.parse::<i32>().unwrap())
+                        .collect::<Vec<i32>>()
+                })
+                .collect::<Vec<Vec<i32>>>()
+        })
+        .collect();
+
+    for num in called_numbers {
+        let mut found = false;
+        for board in boards.iter_mut() {
+            
+            // Set called number
+            board.iter_mut().for_each(|row| {
+                row.iter_mut().for_each(|val| {
+                    if *val == num {
+                        *val = i32::MAX;
+                    }
+                })
+            });
+
+            // validate row win
+            let winning_rows = board.iter()
+                .filter(|row| {
+                    row.iter()
+                        .filter(|&val| *val == i32::MAX)
+                        .count() == 5
+                })
+                .count() > 0;
+
+            // validate col win
+            let mut winning_col = false;
+            for col in 0..board[0].len() {
+                winning_col = true;
+                for row in 0..board.len() {
+                    winning_col &= board[row][col] == i32::MAX
+                }
+                if winning_col {
+                    break;
+                }
+            }
+
+            // validate diagnols
+            let mut winning_diagnol = true;
+            for i in 0..5 {
+                winning_diagnol &= board[i][i] == i32::MAX;
+            }
+
+            let mut winning_diagnol2 = true;
+            let mut j: i32 = 4;
+            for i in 0..5 {
+                winning_diagnol2 &= board[i][j as usize] == i32::MAX;
+                j -= 1;
+            }
+
+            if winning_col || winning_rows || winning_diagnol || winning_diagnol2 {
+                let sum: i32 = board.iter()
+                    .map(|row| row.iter().filter(|&x| *x != i32::MAX).sum::<i32>())
+                    .sum();
+                println!("winning board {}", sum * num);
+                found = true;
+                break;
+            }
+        }
+
+        if found {
+            break;
+        }
+    }
+}
+
+fn day_4_part_2_solution() {
+    let mut input: Vec<String> = fs::read_to_string("input/day_4/part1.txt")
+        .expect("Something went wrong reading the file")
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(String::from)
+        .collect();
+
+    let called_numbers: Vec<i32> = input.remove(0)
+        .split(",")
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+
+    let mut boards: Vec<Vec<Vec<i32>>> = input.chunks(5)
+        .map(|board| {
+            board.iter()
+                .map(|row| {
+                    row.split_whitespace()
+                        .map(|val| val.parse::<i32>().unwrap())
+                        .collect::<Vec<i32>>()
+                })
+                .collect::<Vec<Vec<i32>>>()
+        })
+        .collect();
+
+    let mut won: HashSet<usize> = HashSet::new();
+    for i in 0..boards.len() {
+        won.insert(i);
+    }
+
+    for num in called_numbers {
+        for (idx, board) in boards.iter_mut().enumerate() {
+            if !won.contains(&idx) {
+                continue;
+            }
+
+            // Set called number
+            board.iter_mut().for_each(|row| {
+                row.iter_mut().for_each(|val| {
+                    if *val == num {
+                        *val = i32::MAX;
+                    }
+                })
+            });
+
+            // validate row win
+            let winning_rows = board.iter()
+                .filter(|row| {
+                    row.iter()
+                        .filter(|&val| *val == i32::MAX)
+                        .count() == 5
+                })
+                .count() > 0;
+
+            // validate col win
+            let mut winning_col = false;
+            for col in 0..board[0].len() {
+                winning_col = true;
+                for row in 0..board.len() {
+                    winning_col &= board[row][col] == i32::MAX
+                }
+                if winning_col {
+                    break;
+                }
+            }
+
+            // validate diagnols
+            let mut winning_diagnol = true;
+            for i in 0..5 {
+                winning_diagnol &= board[i][i] == i32::MAX;
+            }
+
+            let mut winning_diagnol2 = true;
+            let mut j: i32 = 4;
+            for i in 0..5 {
+                winning_diagnol2 &= board[i][j as usize] == i32::MAX;
+                j -= 1;
+            }
+
+            if winning_col || winning_rows || winning_diagnol || winning_diagnol2 {
+                if won.len() == 1 {
+                    let sum: i32 = board.iter()
+                    .map(|row| row.iter().filter(|&x| *x != i32::MAX).sum::<i32>())
+                    .sum();
+                    println!("last winning board {}", sum * num);
+                }
+                won.remove(&idx);
+            }
+        }
+    }
 }
